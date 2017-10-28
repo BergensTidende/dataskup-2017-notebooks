6e56a9ca84b9b2197dfd36c34e4d8e3153d1731b
Anders Eriksen <anders.eriksen@bt.no>
2017-10-28 14:50:05 +0200

    Slides

--- scripts/create_slides.sh
+++ scripts/create_slides.sh
@@ -8,10 +8,11 @@ notebooks="$( cd ../notebooks && pwd )"
 slides="$( cd ../slides && pwd )"
 port=8001
 
+cd $slides
+
 for filename in $notebooks/*.ipynb; do
     fileshort=$(basename "${filename%.*}")
     jupyter nbconvert "$filename" --to slides --ServePostProcessor.port=$port --post serve & #$slides/"${fileshort// /_}"
     port=$((port + 1))
 done
 
-cd $slides
