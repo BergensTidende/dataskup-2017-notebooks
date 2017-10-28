c89ada8bf35d07204e5b3ab9dd5715099c3dbd2b
Anders Eriksen <anders.eriksen@bt.no>
2017-10-28 14:05:52 +0200

    Add local server for slides

--- scripts/create_slides.sh
+++ scripts/create_slides.sh
@@ -9,3 +9,6 @@ for filename in $notebooks/*.ipynb; do
     fileshort=$(basename "${filename%.*}")
     jupyter nbconvert "$filename" --to slides --output $slides/"${fileshort// /_}"
 done
+
+cd $slides
+python -m http.server 8000
