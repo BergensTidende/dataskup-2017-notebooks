531c7d221baade6286c11d71f0fc3b1dd689e21a
Anders Eriksen <anders.eriksen@bt.no>
2017-10-28 13:06:26 +0200

    Create slides script

---
+++ scripts/create_slides.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#source activate root
+
+notebooks="$( cd ../notebooks && pwd )"
+slides="$( cd ../slides && pwd )"
+
+for filename in $notebooks/*.ipynb; do
+    fileshort=$(basename "${filename%.*}")
+    jupyter nbconvert "$filename" --to slides --output $slides/"${fileshort// /_}"
+done
